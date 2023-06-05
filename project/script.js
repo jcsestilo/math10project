d617cf57ccbcba98d31998662a27929970630c24
lrlacsy <lrlacsamana4@up.edu.ph>
2023-06-06 00:17:08 +0800

    TAPOS!

--- project/script.js
+++ project/script.js
@@ -1,8 +1,8 @@
 var header = document.getElementById('navitems');
 var logo = document.getElementById('logo');
 var home = document.getElementById('home');
-var about = document.getElementById('about');
-var cont = document.getElementById('cont');
+// var about = document.getElementById('about');
+// var cont = document.getElementById('cont');
 var prev = window.pageYOffset;
 
 window.onscroll = function () {
@@ -12,16 +12,16 @@ window.onscroll = function () {
     header.classList.remove('none');
     logo.classList.remove('invert');
     home.classList.remove('invert');
-    about.classList.remove('invert');
-    cont.classList.remove('invert');
+    // about.classList.remove('invert');
+    // cont.classList.remove('invert');
     header.classList.add('transp');
   } else if (prev > current) {
     header.classList.remove('transp');
     header.classList.remove('none');
     logo.classList.add('invert');
     home.classList.add('invert');
-    about.classList.add('invert');
-    cont.classList.add('invert');
+    // about.classList.add('invert');
+    // cont.classList.add('invert');
     header.classList.add('clr');
   } else {
     header.classList.remove('clr');
